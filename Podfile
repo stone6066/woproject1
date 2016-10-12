2e14835e4dbd790750d62f828af8f5d8287bd56f
关宇琼 <ios_gyq@sina.com>
2016-10-12 09:47:50 +0800

    gs change
    masorny  pch file

--- Podfile
+++ Podfile
@@ -7,5 +7,6 @@ pod 'SDWebImage',    '~>3.7'    #异步加载图片
 pod 'MJRefresh'                 #刷新
 pod 'SVProgressHUD'             #蒙版
 pod 'MBProgressHUD', '~> 0.9.1' #蒙版
+pod 'Masonry'
 end
 
--- Pods/Target Support Files/Pods-woproject/Pods-woproject-frameworks.sh
+++ Pods/Target Support Files/Pods-woproject/Pods-woproject-frameworks.sh
@@ -87,6 +87,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
@@ -94,6 +95,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
